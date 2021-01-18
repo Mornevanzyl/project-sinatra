a841e1ba2f7672c0fcddb671b6b392111e18a888
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-18 17:22:48 +0000

    Introduced parameters and cat year calculations. MiaoW

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 
-
-
-get '/cat' do
-
+get '/random-cat' do
     @catname = ["Amigo", "Oscar", "Viking"].sample
     erb(:index)
 end
+
+get '/named-cat' do
+    p params
+    @catname = params[:name]
+    @age = params[:age]
+    erb(:index)
+end
+
